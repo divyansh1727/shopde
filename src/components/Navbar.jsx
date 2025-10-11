bd3b4a9d2741ba7c32e70bf2692fe2f393410c66
AASHUPA <thakurdivy84@gmail.com>
2025-10-12 01:14:27 +0530

    ss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { FiShoppingBag, FiShoppingCart } from "react-icons/fi";
 export default function Navbar({ cartCount }) {
   return (
     <nav className="bg-black text-white py-4 px-6 flex justify-between items-center fixed w-full z-30 shadow-md">
-      <div className="text-2xl font-bold">Yapper.♡</div>
+      <div className="text-2xl font-bold">Yapper.</div>
       <ul className="flex gap-8 items-center">
         <li>
           <Link to="/" className="flex items-center gap-1 hover:text-gray-300 transition">
